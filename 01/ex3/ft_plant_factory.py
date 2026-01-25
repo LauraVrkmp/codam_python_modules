40d6e9ec0ee44b54548757c04d7d16b69fcdb982
Laura Veerkamp <laveerka@f1r5s6.codam.nl>
2026-01-25 11:20:09 +0100

    need to fix ex4

--- 01/ex3/ft_plant_factory.py
+++ 01/ex3/ft_plant_factory.py
@@ -6,7 +6,7 @@
 #  By: laveerka                                  +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/01/22 10:36:09 by laveerka        #+#    #+#               #
-#  Updated: 2026/01/23 09:54:56 by laveerka        ###   ########.fr        #
+#  Updated: 2026/01/25 09:29:15 by laveerka        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -16,10 +16,21 @@ class Plant:
         self.height = height
         self.days = days
 
-def create_plants():
-    
 
-def print_output(plants: list):
+def create_plants():
+    plants = [Plant("Rose", 25, 3), Plant("Oak", 200, 365),
+              Plant("Cactus", 5, 90), Plant("Sunflower", 80, 45),
+              Plant("Fern", 15, 120)]
+    return plants
+
+
+def print_output(plants: list[Plant]):
+    print("=== Plant factory Output ===")
+    count = 0
+    for plant in plants:
+        print(f"Created: {plant.name} ({plant.height}cm, {plant.days} days)")
+        count += 1
+    print(f"\nTotal plants created: {count}")
 
 
 if __name__ == "__main__":
--- 01/ex4/ft_garden_security.py
+++ 01/ex4/ft_garden_security.py
@@ -6,6 +6,47 @@
 #  By: laveerka                                  +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/01/22 10:36:22 by laveerka        #+#    #+#               #
-#  Updated: 2026/01/22 10:36:23 by laveerka        ###   ########.fr        #
+#  Updated: 2026/01/25 10:14:15 by laveerka        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
+
+class SecurePlant:
+    def __init__(self, name: str, height: int, days: int):
+        self.name = name
+        if height > 0:
+            self.height = height
+        else:
+            print("")
+        if days > 0:
+            self.days = days
+
+    def set_height(self, update):
+        if update > self.height:
+            self.height = update
+            print(f"Height updated: {update}cm [OK]")
+        else:
+            print(f"Invalid operation attempted: height {update}cm [REJECTED]")
+            print("Security: Negative height rejected")
+
+    def set_age(self, update):
+        if update > self.days:
+            self.days = update
+            print(f"Age updated: {update} days [OK]")
+        else:
+            print(f"Invalid operation attempted: age {update} days [REJECTED]")
+            print("Security: Negative days rejected")
+
+    def get_height(self):
+        return self.height
+
+    def get_age(self):
+        return self.days
+
+
+def get_info(plant: SecurePlant):
+    print(f"Current plant: {plant.name} ({plant.get_height}cm, {plant.get_age} days)")
+
+
+if __name__ == "__main__":
+    rose = SecurePlant("Rose", 10, 15)
+    get_info(rose)

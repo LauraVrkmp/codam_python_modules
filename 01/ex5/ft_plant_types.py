c0192adbf13b32ed89a00cc672da1e5553ef3ad6
Laura Veerkamp <laveerka@f1r5s15.codam.nl>
2026-01-28 09:12:20 +0100

    started ex6

--- 01/ex5/ft_plant_types.py
+++ 01/ex5/ft_plant_types.py
@@ -6,7 +6,7 @@
 #  By: laveerka                                  +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/01/22 10:36:35 by laveerka        #+#    #+#               #
-#  Updated: 2026/01/28 06:56:28 by laveerka        ###   ########.fr        #
+#  Updated: 2026/01/28 08:33:45 by laveerka        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -38,8 +38,11 @@ class Tree(Plant):
     def get_type(self) -> str:
         return "Tree"
 
-    def produce_shade(self) -> None:
-        print("Tree is producting shade!")
+    def produce_shade(self, name: str, height: int) -> None:
+        pi = 3.1416
+        h = height / 100
+        shade = pi * h * h
+        print(f"{name} provides {shade} square meters of shade")
 
 
 class Vegetable(Plant):
@@ -57,27 +60,29 @@ def garden_types():
     print("=== Garden Plant Types ===\n")
     rose = Flower("Rose", 25, 30, "red")
     print(f"{rose.name} ({rose.get_type()}): {rose.height}cm, {rose.days} "
-          "days, {rose.color} color")
+          f"days, {rose.color} color")
+    rose.bloom()
     sunflower = Flower("Sunflower", 150, 45, "yellow")
     print(f"{sunflower.name} ({sunflower.get_type()}): {sunflower.height}cm, "
-          "{sunflower.days} days, {sunflower.color} color")
-    rose.bloom()
+          f"{sunflower.days} days, {sunflower.color} color")
+    sunflower.bloom()
     print()
     oak = Tree("Oak", 500, 1825, 50)
     print(f"{oak.name} ({oak.get_type()}): {oak.height}cm, {oak.days} "
-          "days, {oak.trunk_diameter}cm diameter")
-    oak.produce_shade()
+          f"days, {oak.trunk_diameter}cm diameter")
+    oak.produce_shade(oak.name, oak.height)
     birch = Tree("Birch", 350, 1255, 32)
     print(f"{birch.name} ({birch.get_type()}): {birch.height}cm, {birch.days} "
-          "days, {birch.trunk_diameter}cm diameter")
+          f"days, {birch.trunk_diameter}cm diameter")
+    birch.produce_shade(birch.name, birch.height)
     print()
     tomato = Vegetable("Tomato", 80, 90, "summer", "vitamin C")
     print(f"{tomato.name} ({tomato.get_type()}): {tomato.height}cm, "
-          "{tomato.days} days, {tomato.harvest_season} harvest")
+          f"{tomato.days} days, {tomato.harvest_season} harvest")
     print(f"{tomato.name} is rich in {tomato.nutritional_value}")
     sprout = Vegetable("Sprout", 15, 60, "winter", "vitamin K")
     print(f"{sprout.name} ({sprout.get_type()}): {sprout.height}cm, "
-          "{sprout.days} days, {sprout.havest_season} havest")
+          f"{sprout.days} days, {sprout.harvest_season} havest")
     print(f"{sprout.name} is rich in {sprout.nutritional_value}")
 
 
--- 01/ex6/ft_garden_analytics.py
+++ 01/ex6/ft_garden_analytics.py
@@ -6,6 +6,17 @@
 #  By: laveerka                                  +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/01/22 10:36:50 by laveerka        #+#    #+#               #
-#  Updated: 2026/01/22 10:36:51 by laveerka        ###   ########.fr        #
+#  Updated: 2026/01/28 08:41:32 by laveerka        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
+
+class GardenManager:
+    pass
+
+
+def print_demo():
+    print("=== Garden Management System Demo ===")
+
+
+if __name__ == "__main__":
+    print_demo()

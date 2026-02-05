09f2ba30a9e26496baea7ce02292926554ed81f5
Laura Veerkamp <laveerka@f1r5s15.codam.nl>
2026-02-05 11:04:26 +0100

    finished first ex

--- 03/ex0/ft_command_quest.py
+++ 03/ex0/ft_command_quest.py
@@ -6,7 +6,27 @@
 #  By: laveerka                                  +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/02/03 10:33:16 by laveerka        #+#    #+#               #
-#  Updated: 2026/02/03 10:33:18 by laveerka        ###   ########.fr        #
+#  Updated: 2026/02/05 10:38:25 by laveerka        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
+import sys
+
+
+def main():
+    print("=== Command Quest ===")
+    arg_count = len(sys.argv)
+    if arg_count == 1:
+        print("No arguments provided!")
+    print(f"Program name: {sys.argv[0]}")
+    if arg_count > 1:
+        print(f"Arguments received: {arg_count - 1}")
+        iter = 1
+        for argument in sys.argv[1:]:
+            print(f"Argument {iter}: {argument}")
+            iter += 1
+    print(f"Total arguments: {arg_count}")
+
+
+if __name__ == "__main__":
+    main()

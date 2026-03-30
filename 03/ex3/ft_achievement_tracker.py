dd85e74bb27e6e76f1cac9e678da7b1041dbc60e
Laura Veerkamp <laveerka@f0r5s15.codam.nl>
2026-03-30 14:57:20 +0200

    finished achievement tracker system

--- 03/ex3/ft_achievement_tracker.py
+++ 03/ex3/ft_achievement_tracker.py
@@ -6,7 +6,7 @@
 #  By: laveerka                                  +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/02/03 10:33:35 by laveerka        #+#    #+#               #
-#  Updated: 2026/03/26 07:53:50 by laveerka        ###   ########.fr        #
+#  Updated: 2026/03/30 14:56:43 by laveerka        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -20,20 +20,21 @@ class Player:
 
 
 def main():
-    print("=== Achievement Tracker System ===")
-    players = [Player("Alice", {"first kill", "level_10", "treasure_hunter",
+    print("=== Achievement Tracker System ===\n")
+    players = [Player("alice", {"first kill", "level_10", "treasure_hunter",
                                 "speed_demon"}), 
-               Player("Bob", {"first_kill", "level_10", "boss_slayer",
+               Player("bob", {"first_kill", "level_10", "boss_slayer",
                               "collector"}),
-               Player("Charlie", {"level_10", "treasure_hunter",
+               Player("charlie", {"level_10", "treasure_hunter",
                                   "boss_slayer", "speed_demon",
                                   "perfectionist"})]
     for player in players:
-        print(f"Player {player.name} achievements: ")
+        print(f"Player {player.name} achievements: {{", end="")
         for characteristic in player.characterictics:
-            print(f"{characteristic}")
+            print(f"'{characteristic}'", end=" ")
+        print("}")
     print("\n=== Achievement Analytics ===")
 
 
-if __name__ == "__Main__":
+if __name__ == "__main__":
     main()

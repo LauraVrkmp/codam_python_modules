d544e607e4d48bb5e4644edd207c78f04e462f56
Laura Veerkamp <laveerka@f0r5s15.codam.nl>
2026-09-15 11:54:00 +0200

    working on 03/ex5, next(event) doesn't work yet

--- 03/ex5/ft_data_stream.py
+++ 03/ex5/ft_data_stream.py
@@ -3,37 +3,37 @@
 #                                                      :::      ::::::::    #
 #  ft_data_stream.py                                 :+:      :+:    :+:    #
 #                                                  +:+ +:+         +:+      #
-#  By: laveerka <laveerka@student.codam.nl>      +#+  +:+       +#+         #
+#  By: laveerka                                  +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/02/03 10:33:44 by laveerka        #+#    #+#               #
-#  Updated: 2026/09/11 12:44:17 by laveerka        ###   ########.fr        #
+#  Updated: 2026/09/15 11:53:08 by laveerka        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
 import random
 from typing import Generator
 
+
 def gen_event() -> Generator[tuple[str, str], None, None]:
-	players = ["alice", "bob", "charlie", "dylan"]
-	actions = ["climb", "eat", "grab", "move", "release", "run", "sleep", "swim"]
-	for _ in range(10):
-		player = random.choice(players)
-		action = random.choice(actions)
-		yield (player, action)
+    players = ["alice", "bob", "charlie", "dylan"]
+    actions = ["climb", "eat", "grab", "move", "release", "run", "sleep", "swim"]
+    player = random.choice(players)
+    action = random.choice(actions)
+    yield (player, action)
 
 
 def main():
-	print("=== Game Data Stream Processor ===")
-	events = gen_event()
-	iter = 0
-	while True:
-		try:
-			player, action = next(events)
-		except StopIteration:
-			break
-		print(f"Event {iter}: Player {player} did action {action}")
-		iter += 1
+    print("=== Game Data Stream Processor ===")
+    for iter in range(10):
+        event = gen_event()
+        player, action = next(event)
+        print(f"Event {iter}: Player {player} did action {action}")
+        iter += 1
+    events = [str, str]
+    for iter in range(10):
+        event = gen_event()
+        events.append(next(event))
 
 
 if __name__ == "__main__":
-	main()
+    main()

354ff3ea08aa0e5e8dee09f51dc9ae9cc87fc8d6
Laura Veerkamp <laura@HP1-Kali.home>
2026-09-18 10:29:16 +0200

    synch for 04/ex1

--- 04/ex1/ft_archive_creation.py
+++ 04/ex1/ft_archive_creation.py
@@ -6,6 +6,32 @@
 #  By: laveerka <laveerka@student.codam.nl>      +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/09/16 17:41:06 by laveerka        #+#    #+#               #
-#  Updated: 2026/09/16 17:41:07 by laveerka        ###   ########.fr        #
+#  Updated: 2026/09/17 16:16:15 by laveerka        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
+
+import sys
+
+
+def main():
+    args = sys.argv
+    if len(args) == 2:
+        print("==== Cyber Archives Recovery ===")
+        file_name = args[1]
+        print(f"Accessing file '{file_name}'")
+        try:
+            f = open(file_name, "r")
+            print("---\n")
+            content = f.read()
+            print(content)
+            print("\n---")
+            f.close()
+            print(f"File '{file_name}', closed.")
+        except (FileNotFoundError, PermissionError) as e:
+            print(f"Error opening file '{file_name}': {e}")
+            
+    else:
+        print("Usage: ft_ancient_text.py <file>")
+
+if __name__== "__main__":
+    main()

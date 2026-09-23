85dd222ea24f80b406a4652b849e20b73488ad8f
Laura Veerkamp <laveerka@f0r5s9.codam.nl>
2026-09-23 15:50:26 +0200

    started working on 04/ex3

--- 04/ex3/ft_vault_security.py
+++ 04/ex3/ft_vault_security.py
@@ -3,9 +3,28 @@
 #                                                      :::      ::::::::    #
 #  ft_vault_security.py                              :+:      :+:    :+:    #
 #                                                  +:+ +:+         +:+      #
-#  By: laveerka <laveerka@student.codam.nl>      +#+  +:+       +#+         #
+#  By: laveerka                                  +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/09/16 17:41:17 by laveerka        #+#    #+#               #
-#  Updated: 2026/09/16 17:41:19 by laveerka        ###   ########.fr        #
+#  Updated: 2026/09/23 15:49:04 by laveerka        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
+
+def secure_archive(file_name: str, mode: str, content: str | None):
+    with open(file_name, mode[0]) as f:
+        
+    return (True, "true")
+
+
+def main():
+    print("=== Cyber Archives Security ===\n")
+    mode = "read"
+    print(f"Using 'secure_archive' to {mode} from a nonexistent file:")
+    result: tuple(bool, str) = secure_archive("/not/existing/file", mode, None)
+    
+    mode = "write"
+    print(f"Using 'secure_archive' to {mode} previous content to a new file:")
+        
+
+if __name__ == "__main__":
+    main()

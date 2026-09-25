20d4ad9b8d7ffa452acf27d3bb28b04a5036ebc9
Laura Veerkamp <laveerka@f1r4s9.codam.nl>
2026-09-25 14:35:18 +0200

    started 05/ex0

--- 05/ex0/data_processor.py
+++ 05/ex0/data_processor.py
@@ -6,7 +6,41 @@
 #  By: laveerka                                  +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/09/24 13:37:37 by laveerka        #+#    #+#               #
-#  Updated: 2026/09/24 13:37:41 by laveerka        ###   ########.fr        #
+#  Updated: 2026/09/25 14:06:39 by laveerka        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
+from abc import ABC, abstractmethod
+
+
+class DataProcessor(ABC):
+	@abstractmethod
+	def validate(self, data: Any) -> bool:
+
+	@abstractmethod
+	def ingest(self, data: Any) -> None:
+
+	def output(self) -> tuple[int, str]:
+	
+
+
+
+class NumericProcessor(DataProcessor):
+
+
+class TextProcessor(DataProcessor):
+
+
+class LogProcessor(DataProcessor):
+
+
+
+
+def main() -> None:
+	print("=== Code Nexus - Data Processor ===\n")
+	print("Testing Numeric Processor...")
+	
+
+
+if __name__ == "__main__":
+	main()
